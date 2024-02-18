0530978f75955f9173adef507468fff16f9375f5
osparking <jbpark03@gmail.com>
2024-02-18 18:41:11 +0900

    아퀼리언을 사용하는 시험 클래스 제2/2부

--- src/test/java/space/bum/junit/pyram_p2/airport/FlightWithPassengersTest.java
+++ src/test/java/space/bum/junit/pyram_p2/airport/FlightWithPassengersTest.java
@@ -1,5 +1,9 @@
 package space.bum.junit.pyram_p2.airport;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -7,6 +11,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
@@ -21,4 +26,21 @@ public class FlightWithPassengersTest {
   
   @Inject
   Flight flight;
+  
+  @Test(expected = RuntimeException.class)
+  public void testNumberOfSeatsCannotBeExceeded() throws IOException {
+    assertEquals(50, flight.getPassengersNumber());
+    flight.addPassenger(new Passenger("850205-3917188", "정성민", "KR"));
+  }
+  
+  @Test
+  public void testAddRemovePassenters() throws IOException {
+    flight.setSeats(51);
+    Passenger newPassenger = new Passenger("560205-2917188", "정성숙", "KR");
+    flight.addPassenger(newPassenger);
+    assertEquals(51, flight.getPassengersNumber());
+    flight.removePassenger(newPassenger);
+    assertEquals(50, flight.getPassengersNumber());
+    assertEquals(51, flight.getSeats());
+  }
 }
